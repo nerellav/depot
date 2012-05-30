4c10b2be0171db57e16d9425f46f1234ce03b59a
VamsiN <vamsi.nerella@gmail.com>
2012-05-31 00:32:46 +0530

    adding catalog

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ActiveRecord::Base
+  
+  default_scope :order => 'title'
+  
   validates :title, :desc, :presence => true
   validates :title, :uniqueness => true
+  validates :title, :length => {:minimum => 5}
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :image_url, :format => {
     :with => %r{\.(gif|jpg|png|jpeg)$}i,
