ae42158b832f25d4db1231a564a4fb64f6db1b7b
vamsi <vamsi.nerella@gmail.com>
2012-06-02 02:21:12 +0530

    adding cart and line_items

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ActiveRecord::Base
   
   default_scope :order => 'title'
   
+  has_many :line_items
+  
   validates :title, :desc, :presence => true
   validates :title, :uniqueness => true
   validates :title, :length => {:minimum => 5}
@@ -10,4 +12,17 @@ class Product < ActiveRecord::Base
     :with => %r{\.(gif|jpg|png|jpeg)$}i,
     :message => "must be a URL for GIF, JPG or PNG"
   }
+  
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
+  private
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line items present for this Product')
+        return false
+      end
+      
+    end
 end
