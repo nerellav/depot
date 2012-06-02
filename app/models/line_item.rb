a4c82ab3237729c6342fff44d4e2a9fa9f7a92f1
vamsi <vamsi.nerella@gmail.com>
2012-06-02 21:29:04 +0530

    Smarter Cart

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,9 @@ class LineItem < ActiveRecord::Base
   #belongs_to is similar to foreign_key references
   belongs_to :product
   belongs_to :cart
+  
+  def total_price
+    product.price * quantity
+  end
+
 end
