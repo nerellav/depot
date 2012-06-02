36935034a670380a900d0b2af15a3aeb03068cfc
vamsi <vamsi.nerella@gmail.com>
2012-06-02 21:54:08 +0530

    refreshing the cart without ajax

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,10 +1,12 @@
 class StoreController < ApplicationController
+  
   def index
     if session[:counter].nil?
       session[:counter] = 1
     else
       session[:counter] += 1
     end
+    @cart = current_cart
     @products = Product.all
   end
 end
